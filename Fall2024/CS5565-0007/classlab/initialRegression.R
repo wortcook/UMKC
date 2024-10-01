5b3a904dff7c243f454baeaa81424fbb97ddc305
Thomas Jones <thomasjones@Thomass-Mac-Studio.local>
2024-09-30 19:29:10 -0500

    initial regression working

--- Fall2024/CS5565-0007/classlab/initialRegression.R
+++ Fall2024/CS5565-0007/classlab/initialRegression.R
@@ -1,27 +1,22 @@
-#From Wisconsin Breast Cancer Data - Synthetic set #25
-
-#plot(y=factor(synthetic_data_25$diagnosis), x=synthetic_data_25$radius_mean)
-
-#print("Correlation Matrix")
-#cor_matrix = cor(synthetic_data_25[-c(1)])
-#print(cor_matrix)
 
 
-synthetic_data_25$diagnosis_binary <- ifelse(synthetic_data_25$diagnosis=='M',1,0)
+data <- `data_clean`
+#data <- synthetic_data_25
 
-log_model_concavity <- glm(diagnosis_binary ~ radius_mean, data=synthetic_data_25, family=binomial)
+data$diagnosis_binary <- ifelse(data$diagnosis=='M',1,0)
 
-Predicted_data <- data.frame(radius_mean = synthetic_data_25$radius_mean)
+log_model_concavity <- glm(diagnosis_binary ~ radius_mean, data=data, family=binomial)
+
+Predicted_data <- data.frame(radius_mean = seq(min(data$radius_mean), max(data$radius_mean), len=nrow(data)))
 
 print(nrow(synthetic_data_25))
 print(nrow(Predicted_data))
 
 # Fill predicted values using regression model
-Predicted_data$var1 = predict(
-  log_model_concavity, Predicted_data, type="response")
+Predicted_data$var1 = predict(log_model_concavity, Predicted_data, type="response")
 
 # Plot Predicted data and original data points
-plot(y=synthetic_data_25$diagnosis_binary, x=synthetic_data_25$radius_mean)
+plot(y=data$diagnosis_binary, x=data$radius_mean)
 lines(Predicted_data$var1 ~ Predicted_data$radius_mean, Predicted_data, lwd=1, col="green")                           
 
 
