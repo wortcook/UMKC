1be076c0019c5e1c3fad0ef0fa7676f052b8d0ee
Thomas Jones <thomasjones@Thomass-Mac-Studio.local>
2024-10-01 00:52:31 -0500

    all graphs

--- Fall2024/CS5565-0007/classlab/initialRegression.R
+++ Fall2024/CS5565-0007/classlab/initialRegression.R
@@ -1,23 +1,26 @@
 
 
-data <- `data_clean`
-#data <- synthetic_data_25
+#data <- `data_clean`
+data <- synthetic_data_25
 
 data$diagnosis_binary <- ifelse(data$diagnosis=='M',1,0)
 
-log_model_concavity <- glm(diagnosis_binary ~ radius_mean, data=data, family=binomial)
+doLogistic <- function(Y,X,data,xlabel){
+  log_model_concavity <- glm(Y ~ X, data=data, family=binomial)
 
-Predicted_data <- data.frame(radius_mean = seq(min(data$radius_mean), max(data$radius_mean), len=nrow(data)))
-
-print(nrow(synthetic_data_25))
-print(nrow(Predicted_data))
+  Predicted_data <- data.frame(X = seq(min(X), max(X), len=nrow(data)))
 
 # Fill predicted values using regression model
 Predicted_data$var1 = predict(log_model_concavity, Predicted_data, type="response")
 
 # Plot Predicted data and original data points
-plot(y=data$diagnosis_binary, x=data$radius_mean)
-lines(Predicted_data$var1 ~ Predicted_data$radius_mean, Predicted_data, lwd=1, col="green")                           
+  plot(y=Y, x=X, xlab=xlabel)
+  lines(Predicted_data$var1 ~ Predicted_data$X, Predicted_data, lwd=1, col="green")
+}
+
+for( i in 2:ncol(data)){
+  doLogistic(data$diagnosis_binary,data[,i],data,colnames(data)[i])
+}
 
 
 
