24eb56f17561636c9be7153c19cd86506624043e
Thomas Jones <thomasjones@Thomass-Mac-Studio.local>
2024-10-01 06:14:07 -0500

    added lda

--- Fall2024/CS5565-0007/classlab/initialRegression.R
+++ Fall2024/CS5565-0007/classlab/initialRegression.R
@@ -1,4 +1,5 @@
-
+library(ISLR2)
+library(MASS)
 
 #data <- `data_clean`
 data <- synthetic_data_25
@@ -6,20 +7,35 @@ data <- synthetic_data_25
 data$diagnosis_binary <- ifelse(data$diagnosis=='M',1,0)
 
 doLogistic <- function(Y,X,data,xlabel){
-  log_model_concavity <- glm(Y ~ X, data=data, family=binomial)
+  log_model <- glm(Y ~ X, data=data, family=binomial)
 
-  Predicted_data <- data.frame(X = seq(min(X), max(X), len=nrow(data)))
+  log_model.Predicted_data <- data.frame(X = seq(min(X), max(X), len=nrow(data)))
 
-# Fill predicted values using regression model
-Predicted_data$var1 = predict(log_model_concavity, Predicted_data, type="response")
+  # Fill predicted values using regression model
+  Predicted_data$var1 = predict(log_model, Predicted_data, type="response")
 
 # Plot Predicted data and original data points
-  plot(y=Y, x=X, xlab=xlabel)
-  lines(Predicted_data$var1 ~ Predicted_data$X, Predicted_data, lwd=1, col="green")
+#  plot(y=Y, x=X, xlab=xlabel)
+#  lines(Predicted_data$var1 ~ Predicted_data$X, Predicted_data, lwd=1, col="green")
+  
+  return(log_model)
+}
+
+doLDA <- function(Y,X,data){
+  head(X)
+  lda_model <- lda(Y ~ X,data=data)
+  return(lda_model)
 }
 
 for( i in 2:ncol(data)){
-  doLogistic(data$diagnosis_binary,data[,i],data,colnames(data)[i])
+  print('#################')
+  print(colnames(data)[i])
+  print("***** Logistic")
+  print(doLogistic(data$diagnosis_binary,data[,i],data,colnames(data)[i]))
+  print("***** LDA")
+  lda_res = doLDA(data$diagnosis_binary,data[,i],data)
+  plot(lda_res)
+  
 }
 
 
