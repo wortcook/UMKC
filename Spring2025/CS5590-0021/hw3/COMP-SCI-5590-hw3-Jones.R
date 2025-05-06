ecf99a8c779197edf7718731985e7ee3ae0ab39d
Thomas Jones <wortcook@Thomass-MBP.lan>
2025-05-06 17:02:13 -0500

    updates

--- Spring2025/CS5590-0021/hw3/COMP-SCI-5590-hw3-Jones.R
+++ Spring2025/CS5590-0021/hw3/COMP-SCI-5590-hw3-Jones.R
@@ -1,32 +1,43 @@
 #LOAD THE DATA FROM LOCAL DIRECTORY
-df <- read.csv("/Users/thomasjones/workspace/UMKC/Spring2025/CS5590-0021/hw3/car.data.csv")
+#df <- read.csv("/Users/thomasjones/workspace/UMKC/Spring2025/CS5590-0021/hw3/car.data.csv")
+df <- read.csv("/Users/wortcook/Workspace/UMKC/Spring2025/CS5590-0021/hw3/car.data.csv")
 
 head(df)
 
-library(dplyr)
+#1/2
+summary(df[, c("mpg", "cylinders", "displacement", "hp", "weight", "acceleration","modelyr","origin","foreign")])
 
-#Get an idea about some basic feature stats
-feature_stats <- function(df, fname) {
-  print(fname)
-  print(sprintf("mean     : %.1f",mean(df[[fname]],na.rm = TRUE)))
-  print(sprintf("median.  : %.1f",median(df[[fname]],na.rm = TRUE)))
-  print(sprintf("variance : %.1f",var(df[[fname]],na.rm = TRUE)))
-  print(sprintf("min      : %.1f",min(df[[fname]],na.rm = TRUE)))
-  print(sprintf("max      : %.1f",max(df[[fname]],na.rm = TRUE)))
-}
-
-feature_stats(df,"mpg")
-feature_stats(df,"cylinders")
-feature_stats(df, "displacement")
-feature_stats(df, "hp")
-feature_stats(df, "weight")
-feature_stats(df, "acceleration")
-feature_stats(df, "modelyr")
-feature_stats(df, "origin")
-feature_stats(df, "foreign")
+plot(df$cylinders, df$mpg, xlab = "Cylinders", ylab="MPG")
+plot(df$displacement, df$mpg, xlab="Displacement", ylab="MPG")
+plot(df$hp, df$mpg, xlab="HP", ylab="MPG")
+plot(df$weight, df$mpg, xlab="Weight", ylab="MPG")
+plot(df$acceleration, df$mpg, xlab="Acceleration", ylab="MPG")
+plot(df$modelyr, df$mpg, xlab="Model Year", ylab="MPG")
+plot(df$origin, df$mpg, xlab="Origin", ylab="MPG")
 
 
-## Breakdown of foreign, i.e. how many North American origin cars are foreign?
-df_na_foreign <- df %>% filter((origin==1) & (foreign==1))
-head(df_na_foreign)
+df_mod <- df[,!names(df) %in% c("name","foreign")]
+df_mod <- df_mod[, !grepl("^modyr\\d+",names(df_mod))]
+
+#3 - Correlation
+df_mod.cor <- cor(df_mod)
+
+df_mod.cor
+
+#4 - Build Model
+library(car)
+
+model1 = lm(formula = mpg ~ cylinders + displacement + hp + weight + acceleration + modelyr + origin, data = df_mod)
+summary(model1)
+
+vif(model1)
+
+model2 = lm(formula = mpg ~  weight + acceleration + modelyr + origin, data = df_mod)
+summary(model2)
+
+vif(model2)
+
+avPlots(model2)
+
+
 
